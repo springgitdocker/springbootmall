633ae408d44862bdbb297cc112399f0bffbd85da
1116835 <n1116835@ntub.edu.tw>
2025-02-26 16:53:57 +0800

    商品功能 - 實作新增商品

--- src/main/java/com/hannah/springbootmall/dao/ProductDao.java
+++ src/main/java/com/hannah/springbootmall/dao/ProductDao.java
@@ -1,8 +1,10 @@
 package com.hannah.springbootmall.dao;
 
+import com.hannah.springbootmall.dto.ProductRequest;
 import com.hannah.springbootmall.model.Product;
 
 public interface ProductDao {
 
     Product getProductById(Integer productId);
+    Integer createProduct(ProductRequest productRequest);
 }
--- src/main/java/com/hannah/springbootmall/service/ProductService.java
+++ src/main/java/com/hannah/springbootmall/service/ProductService.java
@@ -1,7 +1,10 @@
 package com.hannah.springbootmall.service;
 
+import com.hannah.springbootmall.dto.ProductRequest;
 import com.hannah.springbootmall.model.Product;
 
 public interface ProductService {
     Product getProductById(Integer productId);
+
+    Integer createProduct(ProductRequest productRequest);
 }
