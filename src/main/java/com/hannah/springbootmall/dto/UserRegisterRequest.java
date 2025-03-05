be9e65a2e7efca9714fd95e93446f20a9439eed6
1116835 <n1116835@ntub.edu.tw>
2025-03-05 19:20:05 +0800

    帳號功能 - 檢查註冊的 email, 隱藏返回的密碼

--- src/main/java/com/hannah/springbootmall/dto/UserRegisterRequest.java
+++ src/main/java/com/hannah/springbootmall/dto/UserRegisterRequest.java
@@ -1,10 +1,12 @@
 package com.hannah.springbootmall.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public class UserRegisterRequest {
 
     @NotBlank
+    @Email
     private String email;
     @NotBlank
     private String password;
